7a0856375ab1d6acb45a3dbbdc9f0fcc2e7f4cde
Ali Bozorgzadeh <61201050+abzrg@users.noreply.github.com>
2023-08-18 19:56:57 +0330

    feat(src): add thecherno.cpp and version.cpp
    thecherno.cpp: source code which follows the youtube playlist on opengl
    version.cpp: prints the version of the opengl to the stdout

---
+++ src/version.cpp
@@ -0,0 +1,36 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Create a GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello, OpenGL!", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // Printing version of the openl to the stdout
+    std::cout << glGetString(GL_VERSION) << '\n';
+
+
+    // glfwTerminate();
+    return 0;
+}
